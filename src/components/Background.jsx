e84b8712d898c44f1de7f33f168329be181406be
Jon Cheuvront <jon.cheuvront@pm.me>
2025-03-25 20:36:49 -0400

    Cha Cha Cha Changes!!!!!

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,7 +1,7 @@
 export default function Background({ artwork }) {
   return (
     <div className={`bg-[url(${artwork})] size-full bg-cover bg-center bg-no-repeat`}>
-      <div className="size-full text-white backdrop-blur-3xl" />
+      <div className="size-full text-black backdrop-blur-3xl" />
     </div>
   );
 }
--- src/components/TrackDetails.jsx
+++ src/components/TrackDetails.jsx
@@ -4,7 +4,7 @@ export default function TrackDetails({ artist, artwork, album, title }) {
       <img src={artwork} className="-mt-2 rounded-md drop-shadow-2xl" />
       <div className="mt-8 flex flex-col items-center justify-center">
         <p className="mb-4 text-5xl font-bold text-slate-100 drop-shadow-lg">{title}</p>
-        <p className="text-2xl font-light text-white drop-shadow-lg">{artist}</p>
+        <p className="text-2xl font-light text-white drop-shadow-lg">{artist} - {album}</p>
       </div>
     </div>
   );
